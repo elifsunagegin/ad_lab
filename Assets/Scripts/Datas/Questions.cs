271954b74c2a3d3f1b3508eaa8c70e2678698073
Hasan Buğra Duru <hasanbugraduru@hotmail.com>
2024-04-18 00:50:17 +0300

    Question Screen

--- Assets/Scripts/Datas/Questions.cs
+++ Assets/Scripts/Datas/Questions.cs
@@ -1,20 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [CreateAssetMenu(fileName ="Questions",menuName ="Questions")]
 public class Questions : ScriptableObject
 {
     [Header("Config")]
     [Header("Questions")]
-    [SerializeField] private string[] questions;
-    [SerializeField] private string[] OP1;
-    [SerializeField] private string[] OP2;
-    [SerializeField] private string[] OP3;
-    [SerializeField] private string[] OP4;
-    [SerializeField] private bool[] TrueOnes;
-    [SerializeField] private bool[] SelectedOnes;
+    public int SelectedCategorie;
+    [SerializeField] public string[] questions;
+    [SerializeField] public string[] OP1;
+    [SerializeField] public string[] OP2;
+    [SerializeField] public string[] OP3;
+    [SerializeField] public string[] OP4;
+    [SerializeField] public Image[] Images;
+    [SerializeField] public bool[] TrueOnes;
+    [SerializeField] public bool[] SelectedOnes;
 
+    [SerializeField] public int SelectedQuestion;
    
      
   
