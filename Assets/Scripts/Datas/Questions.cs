eaf1fb2ec70485862cccde6498e773b81854735d
Hasan Buğra Duru <hasanbugraduru@hotmail.com>
2024-04-18 14:29:11 +0300

    Selected Question

--- Assets/Scripts/Datas/Questions.cs
+++ Assets/Scripts/Datas/Questions.cs
@@ -8,12 +8,18 @@ public class Questions : ScriptableObject
 {
     [Header("Config")]
     [Header("Questions")]
+<<<<<<< Updated upstream
     public int SelectedCategorie;
+=======
+    [SerializeField] public int SelectedCategorie;
+    [SerializeField] public int SelectedQuestion;
+>>>>>>> Stashed changes
     [SerializeField] public string[] questions;
     [SerializeField] public string[] OP1;
     [SerializeField] public string[] OP2;
     [SerializeField] public string[] OP3;
     [SerializeField] public string[] OP4;
+<<<<<<< Updated upstream
     [SerializeField] public Image[] Images;
     [SerializeField] public bool[] TrueOnes;
     [SerializeField] public bool[] SelectedOnes;
@@ -21,7 +27,11 @@ public class Questions : ScriptableObject
     [SerializeField] public int SelectedQuestion;
    
      
-  
+=======
+    [SerializeField] public bool[] TrueOnes;
+    [SerializeField] public bool[] SelectedOnes;
 
+>>>>>>> Stashed changes
+  
 
 }
