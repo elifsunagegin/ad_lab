ced67746e39cd9678e547b65cf2e200f1818e311
sunaggn <sunagegin@gmail.com>
2024-05-22 02:41:51 +0300

    achievements unitest update
    unitest update

--- Assets/Scripts/Datas/Tests/NewTestScript.cs
+++ Assets/Scripts/Datas/Tests/NewTestScript.cs
@@ -1,25 +1,58 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
+using UnityEngine.UI;
 
 public class NewTestScript
 {
     // A Test behaves as an ordinary method
     [Test]
-    public void NewTestScriptSimplePasses()
+    public void AchievementUnlocked_WhenPlayTimeOverFifteenHours()
     {
-        // Use the Assert class to test conditions
+        var playerData = ScriptableObject.CreateInstance<PlayerDatas>();
+        playerData.PlayTime = 10; // Simulate play time under fifteen hours
+        var achievementManager = new AchievementManager();
+        achievementManager.PlayerDatas = playerData;
+        achievementManager.Lock = new GameObject().AddComponent<Image>(); // Simulate Lock Image
+
+        // Act
+        achievementManager.Start();
+
+        // Assert
+        Assert.IsTrue(achievementManager.Lock.gameObject.activeSelf, "Lock should be activated when play time is under fifteen hours.");
     }
 
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
+    
     [UnityTest]
     public IEnumerator NewTestScriptWithEnumeratorPasses()
     {
         // Use the Assert class to test conditions.
         // Use yield to skip a frame.
         yield return null;
+    }
+}
+
+[CreateAssetMenu(fileName = "PlayerDatas", menuName = "ScriptableObjects/PlayerDatas", order = 1)]
+public class PlayerDatas : ScriptableObject
+{
+    public int PlayTime { get; set; }
+}
+
+
+internal class AchievementManager
+{
+    public AchievementManager()
+    {
+    }
+
+    public PlayerDatas PlayerDatas { get; internal set; }
+    public Image Lock { get; internal set; }
+
+    internal void Start()
+    {
+        throw new NotImplementedException();
     }
 }
